760c3eed516277fd6b5ac5fadfe3bfcd70660500
siri nerella <sirinerella44@gmail.com>
2024-07-03 22:35:03 +0530

    variable declaration

---
+++ variable declaration.py
@@ -0,0 +1,18 @@
+# variable= variable is container it can stores the values
+# 
+a ="siri"
+print(a)
+# int : it stores a whole integer number and
+s =20
+print("s")
+print(type(s))
+#  float : float data type is a numeric vlue that can stores a number that includes a decimal portion int data
+height =20.2
+print(height)
+# commenting
+# single line comment: start with #
+# multiline comment:''' siri'''
+# memory indexing
+a ="siri"
+v =a[0]
+print(v)
